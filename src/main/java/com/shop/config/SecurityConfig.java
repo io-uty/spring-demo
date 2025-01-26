4b22db63e4bcda18d4cfab4b00758add47120960
io-uty <hyparksj@inha.edu>
2025-01-26 16:43:40 +0900

    로그인, 로그아웃 기능 구현

--- src/main/java/com/shop/config/SecurityConfig.java
+++ src/main/java/com/shop/config/SecurityConfig.java
@@ -1,15 +1,27 @@
 package com.shop.config;
 
+import com.shop.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    private final MemberService memberService;
+
+    public SecurityConfig(MemberService memberService) {
+        this.memberService = memberService;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -21,11 +33,14 @@ public class SecurityConfig {
                 )
                 .formLogin(form -> form
                         .loginPage("/login") // 사용자 정의 로그인 페이지 설정
+                        .defaultSuccessUrl("/")
+                        .usernameParameter("email")
+                        .failureUrl("/members/login/error") // 오타 수정
                         .permitAll()
                 )
                 .logout(logout -> logout
-                        .logoutUrl("/logout") // 로그아웃 URL 설정
-                        .logoutSuccessUrl("/home") // 로그아웃 성공 시 이동 경로
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
+                        .logoutSuccessUrl("/") // 로그아웃 성공 시 이동 경로
                         .permitAll()
                 );
 
@@ -35,5 +50,10 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
--- src/main/java/com/shop/controller/MemberController.java
+++ src/main/java/com/shop/controller/MemberController.java
@@ -3,25 +3,24 @@ package com.shop.controller;
 import com.shop.dto.MemberFormDto;
 import com.shop.entity.Member;
 import com.shop.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/members")
 @Controller
+@RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
     private final PasswordEncoder passwordEncoder;
-
-    // 생성자 주입
-    public MemberController(MemberService memberService, PasswordEncoder passwordEncoder) {
-        this.memberService = memberService;
-        this.passwordEncoder = passwordEncoder;
-    }
 
     @PostMapping(value = "/new")
     public String newMember(@Valid MemberFormDto memberFormDto,
@@ -37,5 +36,17 @@ public class MemberController {
             return "member/memberForm";
         }
         return "redirect:/";
+    }
+
+
+    @GetMapping(value="/login")
+    public String loginMember(){
+        return "/member/memberLoginForm";
+    }
+
+    @GetMapping(value = "/login/error")
+    public String loginError(Model model){
+        model.addAttribute("loginErrorMsg","아이디 또는 비밀번호를 확인해주세요");
+        return "/member/memberLoginForm";
     }
 }
